02d94503ad67758ba30e9c9e5ed02435cd0697dc
etermaw <etermaw@gmail.com>
2017-11-25 14:23:22 +0100

    added db model

---
+++ database/models/DatabaseModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace database.models
+{
+    class DatabaseModel
+    {
+        private SqlConnection connection;
+
+        public DatabaseModel()
+        {
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|data.mdf;Integrated Security=True");
+        }
+
+        public string TryConnect()
+        {
+            try
+            {
+                connection.Open();
+            }
+
+            catch(SqlException e)
+            {
+                return e.Message;
+            }
+
+            return null;
+        }
+    }
+}
