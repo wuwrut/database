0acedbd8552170e284173a4a244530511911bffa
wad321y <wad321y@gmail.com>
2017-12-06 17:24:59 +0100

    Minor changes to .csproj

--- database/views/User.xaml.cs
+++ database/views/User.xaml.cs
@@ -27,10 +27,5 @@ namespace database
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
