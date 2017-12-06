11d9ccde36a775dfa022c9aeb3ec43f5a3ef1403
wad321y <wad321y@gmail.com>
2017-12-06 17:31:25 +0100

    Adding skeleton

---
+++ database/view-models/ViewShowData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace database.view_models
+{
+    class ViewShowData
+    {
+    }
+}
